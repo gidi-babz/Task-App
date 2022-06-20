098047d160ffc84be1c288022b39357f4215b551
Gidi Babz <tunex4ind@gmail.com>
2022-06-20 20:39:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+	const [tasks, setTasks] = useState([]);
+
+	const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+	useEffect(() => {
+		const transformedTasks = (taskObj) => {
+			const loadedTasks = [];
+
+			for (const taskKey in taskObj) {
+				loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+			}
+
+			setTasks(loadedTasks);
+		};
+
+		fetchTasks(
+			{
+				url: 'https://react-fetch-3ecf4-default-rtdb.firebaseio.com/tasks.json',
+			},
+			transformedTasks
+		);
+	}, [fetchTasks]);
+
+	const taskAddHandler = (task) => {
+		setTasks((prevTasks) => prevTasks.concat(task));
+	};
+
+	return (
+		<React.Fragment>
+			<NewTask onAddTask={taskAddHandler} />
+			<Tasks
+				items={tasks}
+				loading={isLoading}
+				error={error}
+				onFetch={fetchTasks}
+			/>
+		</React.Fragment>
+	);
+}
+
+export default App;
